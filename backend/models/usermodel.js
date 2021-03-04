b0102202431e2f7a7278391f47a14e0daacc2b91
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2021-03-04 14:55:28 +0530

    mongodb cluster

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -4,29 +4,24 @@ const userSchema = new mongoose.Schema({
     
     email:{
         type:String,
-     required:true
-    }
-    // firstname:{
-    //     type:String,
-    //     required:true
-    // },
-    // surname:{
-    //     type:String,
-    //     required:true
-    // },
-    // phone:{
-    //     type:Number,
-    //     required:true
-    // },
-    // password:{
-    //     type:String,
-    //     required:true
-    // },
-    // dob:{
-    //     type:String,
-    //     required:true
-    // },
-    // friends:[{type:Object}]  
+    },
+    firstname:{
+        type:String,
+       
+    },
+    lastname:{
+        type:String,
+        
+    },
+    password:{
+        type:String,
+        
+    },
+    dob:{
+        type:String,
+        required:true
+    },
+    friends:[{type:Object}]  
 })
 const user = mongoose.model('userdata',userSchema)
 module.exports =user
--- frontend/src/Mainrouter.js
+++ frontend/src/Mainrouter.js
@@ -6,7 +6,7 @@ import Navbar from './Components/Nav/Navbar';
 import Newsfeed from './Components/Newsfeed/Newsfeed';
 import Rough from './Rough';
 import Error from './Components/Error'
-
+import Register from './Components/Register/Register'
 function Mainrouter() {
   return (
     <div className="App">
@@ -15,6 +15,7 @@ function Mainrouter() {
         <Route path='/'exact component={Homepage}/>
         <Route path='/profile'  component={Userprofile}/>
         <Route path='/feed' component={Newsfeed}/>
+        <Route path='/register' component={Register}/>
         <Route path='/play' component={Rough}/>
       </BrowserRouter>
     </div>
